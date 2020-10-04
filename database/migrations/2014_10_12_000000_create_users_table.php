27480c84ffb3d8eda2f40a87096426ebf5654c2b
WahyuS002 <syahputrawahyu61@gmail.com>
2020-10-04 10:29:23 +0700

    laravel\ui + users migartion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama');
+            $table->char('nip', 18);
             $table->string('password');
+            $table->enum('jk', ['l', 'p']);
+            $table->string('alamat');
+            $table->string('no_hp', 15);
+            $table->string('foto_profil');
             $table->rememberToken();
             $table->timestamps();
         });
