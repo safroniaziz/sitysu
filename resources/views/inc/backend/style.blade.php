eacb844a6fde142259f72826511e6a86b5ff412e
WahyuS002 <syahputrawahyu61@gmail.com>
2020-10-05 20:59:09 +0700

    admin dashborad

--- resources/views/inc/backend/style.blade.php
+++ resources/views/inc/backend/style.blade.php
@@ -2,4 +2,5 @@
 <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 <link href="{{ asset('cork/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('cork/assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('cork/assets/css/structure-custom.css') }}" rel="stylesheet" type="text/css" />
 <!-- END GLOBAL MANDATORY STYLES -->
