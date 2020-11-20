0e5d616bf98c816e81156934ef86f7b8fa7754de
WahyuS002 <syahputrawahyu61@gmail.com>
2020-11-20 08:45:41 +0700

    override method authenticateUsers in loginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,11 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function username()
+    {
+        return 'nip';
+    }
+
     /**
      * Where to redirect users after login.
      *
